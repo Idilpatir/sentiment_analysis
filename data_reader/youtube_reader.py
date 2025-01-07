7c16dc45440861cee3fe1df86ce875ec8b773798
İdil <idilpatir@hotmail.com>
2025-01-07 23:58:27 +0300

    add type hints

--- data_reader/youtube_reader.py
+++ data_reader/youtube_reader.py
@@ -2,17 +2,17 @@ import requests
 from urllib.parse import urlparse, parse_qs
 
 class Youtube_Reader:
-    def __init__(self, API_KEY) -> None:
+    def __init__(self, API_KEY: str) -> None:
         self.apikey = API_KEY
 
 
-    def _Parse(self, video_url):
+    def _Parse(self, video_url: str)-> str:
         url=urlparse(video_url)
         video_id = parse_qs(url.query)['v'][0]
         return video_id
     
 
-    def Read(self, video_url, count=20):
+    def Read(self, video_url: str, count: int=20) -> list[str]:
         video_id = self._Parse(video_url)
         payload = {
             "key" : self.apikey,
--- sentiment_analysis/sentimet_analysis.py
+++ sentiment_analysis/sentimet_analysis.py
@@ -5,13 +5,10 @@ class Sentiment_Analyzer:
     def __init__(self) -> None:
         self.Analyzer = SentimentIntensityAnalyzer()
 
-    def _Clean(self,text):
+    def _Clean(self,text: str) -> str:
         return " ".join(re.sub(r'[^\w?!]', ' ', text).split())   ####
 
-    
-
-
-    def Analyze(self, text):
+    def Analyze(self, text: str) -> int:
         clean_text = self._Clean(text)
         score = self.Analyzer.polarity_scores(clean_text)
         comp = score["compound"]
