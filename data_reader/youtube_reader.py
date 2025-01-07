515f9bbfcb950253bad7c38b3dc3f7bec0d0e5c8
İdil <idilpatir@hotmail.com>
2025-01-07 23:41:00 +0300

    wrap up methods

--- data_reader/youtube_reader.py
+++ data_reader/youtube_reader.py
@@ -1,11 +1,19 @@
 import requests
-
+from urllib.parse import urlparse, parse_qs
 
 class Youtube_Reader:
     def __init__(self, API_KEY) -> None:
         self.apikey = API_KEY
 
-    def Read(self, video_id, count=20):
+
+    def _Parse(self, video_url):
+        url=urlparse(video_url)
+        video_id = parse_qs(url.query)['v'][0]
+        return video_id
+    
+
+    def Read(self, video_url, count=20):
+        video_id = self._Parse(video_url)
         payload = {
             "key" : self.apikey,
             "maxResults" : count,
--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from dotenv import load_dotenv, dotenv_values
 from sentiment_analysis.sentimet_analysis import Sentiment_Analyzer
 load_dotenv()
 
-Analyzer = Sentiment_Analyzer()
-print(Analyzer.Analyze("shit.a !!!!"))
-print(Analyzer.Analyze("very bad!!"))
-print(Analyzer.Analyze("avarage"))
-print(Analyzer.Analyze("good"))
-print(Analyzer.Analyze("very good!?!?"))
+if __name__ == "__main__":
+    API_KEY = dotenv_values(".env")["API_KEY"]
+    reader = Youtube_Reader(API_KEY)
+    analyzer = Sentiment_Analyzer()
+
+    video_id = input("Enter video url: ")
+
+    comments = reader.Read(video_id)
+
+    for comment in comments:
+        score = analyzer.Analyze(comment)
+        print(f"Comment: {comment}")
+        print(f"Score: {score}")
+
+
